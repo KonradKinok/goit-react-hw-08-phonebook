76a4a61e418890e0665dcf01b097524e3a02a0ac
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-12-11 12:22:05 +0100

    11.12.2024

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//  plugins: [react()],
+//  base: "",
+//  build: {
+//   outDir: "./dist",
+//  },
+// });
+
 export default defineConfig({
  plugins: [react()],
- base: "",
  build: {
-  outDir: "./dist",
+  sourcemap: true,
  },
 });
